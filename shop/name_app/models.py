916bd2d60a1b7a0cffe07edf7f4ea30488406565
Damian Malik <malikdamiann@gmail.com>
2021-09-22 11:35:07 +0200

    add models

--- shop/name_app/models.py
+++ shop/name_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    producer = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.FloatField()
+    photo = models.ImageField(null=True)
+
+
+class Order(models.Model):
+    client = models.CharField(max_length=128)
+    products = models.ManyToManyField(Product)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_payment = models.DateField()
+    total_price = models.FloatField()
+
+
+class Address(models.Model):
+    username = models.CharField(max_length=128)
+    company = models.CharField(max_length=128, null=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    house_number = models.IntegerField()
+    premises_number = models.IntegerField(null=True)
+    postcode = models.CharField(max_length=128)
+    location = models.CharField(max_length=128)
+
+
+
