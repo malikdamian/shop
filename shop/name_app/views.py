f816fa28003c16a4410d53ccdfe1c7d0491a6179
Damian Malik <malikdamiann@gmail.com>
2021-09-23 19:15:29 +0200

    create ProductsView

--- shop/name_app/views.py
+++ shop/name_app/views.py
@@ -1,3 +1,27 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
+from django.views import View
+from .models import Product
 
-# Create your views here.
+
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'base.html')
+
+
+class ProductsView(View):
+
+    def get(self, request):
+        search = request.GET.get('search', '')
+        if search:
+            products = Product.objects.filter(Q(name__icontains=search) | Q(producer__icontains=search))
+        else:
+            products = Product.objects.all()
+        paginator = Paginator(products, 3)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'products.html', {'page_obj': page_obj,
+                                                 'search': search})
+
