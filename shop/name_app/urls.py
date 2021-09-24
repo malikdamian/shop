31f901546adc3bd32215fb5bb6709eefb88f36fd
Damian Malik <malikdamiann@gmail.com>
2021-09-24 10:59:53 +0200

    add product url

--- shop/name_app/urls.py
+++ shop/name_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import IndexView, ProductsView
+from .views import IndexView, ProductsView, ProductView
 
 app_name = 'name_app'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('products/', ProductsView.as_view(), name='products'),
+    path('product/<int:product_id>/', ProductView.as_view(), name='product')
 
 
 ]
