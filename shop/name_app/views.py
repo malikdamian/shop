5a7e9e486ae77163a94806432ee0d06c150ed610
Damian Malik <malikdamiann@gmail.com>
2021-09-27 11:21:25 +0200

    feature add product

--- shop/name_app/views.py
+++ shop/name_app/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from .forms import ProductForm
 from .models import Product
 
 
@@ -19,8 +20,8 @@ class ProductsView(View):
             products = Product.objects.filter(Q(name__icontains=search) |
                                               Q(producer__icontains=search))
         else:
-            products = Product.objects.all()
-        paginator = Paginator(products, 2)
+            products = Product.objects.all().order_by('-id')
+        paginator = Paginator(products, 3)
         page_number = request.GET.get('page', 1)
         page_obj = paginator.get_page(page_number)
         return render(request, 'products.html', {'page_obj': page_obj,
@@ -35,3 +36,16 @@ class ProductView(View):
         return render(request, 'product.html', {'product': product,
                                                 'products': products})
 
+
+class AddProductView(View):
+
+    def get(self, request):
+        form = ProductForm()
+        return render(request, 'base_form.html', {'form': form})
+
+    def post(self, request):
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/products/')
+        return render(request, 'base_form.html', {'form': form})
