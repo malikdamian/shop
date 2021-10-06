1897c4106b937be286c17eff7404bb81881a18cd
Damian Malik <malikdamiann@gmail.com>
2021-10-06 15:26:16 +0200

    added models admin

--- shop/name_app/admin.py
+++ shop/name_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product, Customer, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'producer', 'description', 'price', 'image')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ShippingAddress)
+class ShippingAddressAdmin(admin.ModelAdmin):
+    pass
